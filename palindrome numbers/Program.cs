210c5bcdca8ebca2db6a3dcf888c9b8b125e935d
nomvula <nzshabangu95@gmail.com>
2023-09-14 00:03:04 +0200

    Add project files.

---
+++ palindrome numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (IsPalindrome(number))
+            {
+                Console.WriteLine(number + " is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a palindrome.");
+            }
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(int number)
+        {
+            int originalNumber = number;
+            int reverseNumber = 0;
+
+            while (number != 0)
+            {
+                int remainder = number % 10;
+                reverseNumber = reverseNumber * 10 + remainder;
+                number /= 10;
+            }
+
+            return originalNumber == reverseNumber;
+        }
+        
+    }
+}
